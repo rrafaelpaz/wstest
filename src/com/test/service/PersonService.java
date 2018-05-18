a714e4c202050e7aa7ddfbe51545d68f8bd6410c
Rafael Paz <rrafaelpaz@gmail.com>
2018-05-18 19:50:14 +1000

    first commiting

---
+++ src/com/test/service/PersonService.java
@@ -0,0 +1,14 @@
+package com.test.service;
+
+import com.test.model.Person;
+import com.test.model.Response;
+
+public interface PersonService {
+
+	
+	public Response addPerson(Person p);
+	public Response deletePerson(int id);
+	public Person getPerson(int id);
+	public Person[] getAllPersons();
+	
+}
